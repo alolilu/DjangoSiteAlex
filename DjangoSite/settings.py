ed0cd2f662fd571209801cec5fb7206c82d5d20f
ALEX <alexis.fredriksen5@gmail.com>
2021-04-10 12:56:46 +0200

    0

--- DjangoSite/settings.py
+++ DjangoSite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'alexis.fredriksen5@gmail.com'
-EMAIL_HOST_PASSWORD = 'Fripons.154'
+
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
